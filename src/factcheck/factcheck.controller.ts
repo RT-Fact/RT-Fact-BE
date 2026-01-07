cef4b730c0aba7c6c767573413bee50f9bf4e971
taehoonkim1007 <dsst1007@gmail.com>
2026-01-07 21:03:48 +0900

    feat: 전역 JWT 가드 적용 및 @Public 데코레이터 구현 - JwtAuthGuard를 전역 가드(APP_GUARD)로 설정하여 기본 보안 강화 - 인증 예외 처리를 위한 @Public 데코레이터 구현

--- src/factcheck/factcheck.controller.ts
+++ src/factcheck/factcheck.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { Body, Controller, HttpCode, HttpStatus, Post, Request } from "@nestjs/common";
 import { CreateFactCheckDto } from "./dto/create-factcheck.dto";
 import { FactCheckResponse } from "./dto/factcheck-response.dto";
 import { FactCheckService } from "./factcheck.service";
@@ -10,7 +9,6 @@ export class FactCheckController {
   constructor(private readonly factCheckService: FactCheckService) {}
 
   @Post()
-  @UseGuards(AuthGuard("jwt"))
   @HttpCode(HttpStatus.CREATED)
   async create(
     @Request() req: RequestWithUser,
