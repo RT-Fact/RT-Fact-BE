cf4a787c5c980e94ac7591c38c9653c476413212
SangJin983 <lazistar983@gmail.com>
2026-01-17 18:22:59 +0900

    feat: API 키 이름 생성 규칙 완화 (정규식 제거)

--- src/api-keys/dto/create-api-key.dto.ts
+++ src/api-keys/dto/create-api-key.dto.ts
@@ -1,10 +1,7 @@
-import { IsString, Length, Matches } from "class-validator";
+import { IsString, Length } from "class-validator";
 
 export class CreateApiKeyDto {
   @IsString()
   @Length(1, 50)
-  @Matches(/^[a-zA-Z0-9\s\-_]+$/, {
-    message: "이름은 영문, 숫자, 공백, 하이픈(-), 언더바(_)만 포함할 수 있습니다.",
-  })
   name: string;
 }
